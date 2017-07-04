f9059040e9b1410f6a44c65f6b0f645b4bf9203c
John Munsch <john.munsch@gmail.com>
2017-07-04 16:51:52 -0500

    Starting to flesh out a Gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,44 @@
 'use strict';
 
 const gulp = require('gulp');
-const cp = require('child_process');
-// const concat = require('gulp-concat');
-// const uglify = require('gulp-uglify');
-// const rename = require('gulp-rename');
-// const cleanCSS = require('gulp-clean-css');
-// const del = require('del');
+const exec = require('child_process').execSync;
+const del = require('del');
 
-// var paths = {
-//   styles: {
-//     src: 'src/styles/**/*.less',
-//     dest: 'assets/styles/'
-//   },
-//   scripts: {
-//     src: 'src/scripts/**/*.js',
-//     dest: 'assets/scripts/'
-//   }
-// };
-//
-// /* Not all tasks need to use streams, a gulpfile is just another node program
-//  * and you can use all packages available on npm, but it must return either a
-//  * Promise, a Stream or take a callback and call it
-//  */
-// function clean() {
-//   // You can use multiple globbing patterns as you would with `gulp.src`,
-//   // for example if you are using del 2.0 or above, return its promise
-//   return del([ 'assets' ]);
-// }
-//
-// /*
-//  * Define our tasks using plain functions
-//  */
-// function styles() {
-//   return gulp.src(paths.styles.src)
-//     .pipe(less())
-//     .pipe(cleanCSS())
-//     // pass in options to the stream
-//     .pipe(rename({
-//       basename: 'main',
-//       suffix: '.min'
-//     }))
-//     .pipe(gulp.dest(paths.styles.dest));
-// }
-//
-// function scripts() {
-//   return gulp.src(paths.scripts.src, { sourcemaps: true })
-//     .pipe(babel())
-//     .pipe(uglify())
-//     .pipe(concat('main.min.js'))
-//     .pipe(gulp.dest(paths.scripts.dest));
-// }
-//
-// function watch() {
-//   gulp.watch(paths.scripts.src, scripts);
-//   gulp.watch(paths.styles.src, styles);
-// }
-//
-// /*
-//  * You can use CommonJS `exports` module notation to declare tasks
-//  */
-// exports.clean = clean;
-// exports.styles = styles;
-// exports.scripts = scripts;
-// exports.watch = watch;
-//
-// /*
-//  * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
-//  */
-// var build = gulp.series(clean, gulp.parallel(styles, scripts));
-//
-// /*
-//  * You can still use `gulp.task` to expose tasks
-//  */
-// gulp.task('build', build);
-//
-// /*
-//  * Define default task that can be called by just running `gulp` from cli
-//  */
-// gulp.task('default', build);
+let target = {
+  clean: [
+    './dist/'
+  ],
+  sourceRoot: '../../app',
+  dist: './dist/'
+};
 
+gulp.task('clean', function () {
+  return del(target.clean);
+});
+
+// autoprefixer
+// usemin
+// - cache busting via JS and CSS file renaming
+// copy files to dist directory
+
+// Ops tasks - All of these use Ansible to do their work.
 gulp.task('ops:configure', function() {
   // In gulp 4, you can return a child process to signal task completion
-  return cp.exec('ansible-playbook -b configure.yml', {
+  return exec('ansible-playbook -b configure.yml', {
     cwd: 'ops'
   });
 });
 
 gulp.task('ops:deploy', function () {
-  return cp.exec('ansible-playbook -b deploy.yml', {
+  return exec('ansible-playbook -b deploy.yml', {
     cwd: 'ops'
   });
 });
 
 gulp.task('ops:upgrade', function () {
-  return cp.exec('ansible-playbook -b upgrade.yml', {
+  return exec('ansible-playbook -b upgrade.yml', {
     cwd: 'ops'
   });
 });
+
+// gulp.task('default', gulp.parallel());
